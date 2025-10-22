1bfbf6b0f288424073343b51c9d1d04dba451687
Maré Viva <odsmareviva@gmail.com>
2025-10-22 18:36:44 -0300

    correção app.js

--- app.js
+++ app.js
@@ -4,22 +4,21 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.use('/fontawesome', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
-app.use(express.static('./app/public'));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, 'app', 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var rotas = require('./app/routes/router');
+const rotas = require('./app/routes/router');
 app.use('/', rotas);
 
 try {
-    app.listen(port, e => {
-        console.log(`Servidor online\nHttp://localhost:${port}`);
-        if (e)
-            console.error(e);
+    app.listen(port, err => {
+    if (err) console.error(err);
+    else console.log(`Servidor online em http://localhost:${port}`);
     });
 } catch (error) {
     console.log("Erro ao iniciar o servidor: " + error);
