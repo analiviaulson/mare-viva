cda9a4f257c25a13c0b8b922b45648e11bdb1e5a
Maré Viva <odsmareviva@gmail.com>
2025-10-22 18:29:31 -0300

    correção render

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use('/fontawesome', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
 app.use(express.static('./app/public'));
 
 app.set('view engine', 'ejs');
-app.set('views', './app/views');
+app.set('views', path.join(__dirname, 'app/views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
